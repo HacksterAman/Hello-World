53372902512c84510467f1bd1b1c3c1ec572b679
Aman Singh <142312697+HacksterAman@users.noreply.github.com>
2023-11-04 11:32:01 +0530

    Update Hello World.py

--- Hello World.py
+++ Hello World.py
@@ -8,4 +8,5 @@ for char in "Hello World!":
     	   print(result + random.choice(string.ascii_letters), end='\r')
     	   time.sleep(0.1)
     	result += char
+    	time.sleep(0.1)
     	print(result,end='\r')
