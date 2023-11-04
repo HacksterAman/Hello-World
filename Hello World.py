0daafb6c194cc9e3b46e18f486bfe46e32a77be7
Aman Singh <142312697+HacksterAman@users.noreply.github.com>
2023-11-04 12:39:53 +0530

    Update Hello World.py

--- Hello World.py
+++ Hello World.py
@@ -2,11 +2,28 @@ import time
 import random
 import string
 
+colors = [
+    "\033[91m",  # Red
+    "\033[92m",  # Green
+    "\033[93m",  # Yellow
+    "\033[94m",  # Blue
+    "\033[95m",  # Magenta
+    "\033[96m",  # Cyan
+    "\033[31m",  # Light Red
+    "\033[32m",  # Light Green
+    "\033[33m",  # Light Yellow
+    "\033[34m",  # Light Blue
+    "\033[35m",  # Light Magenta
+    "\033[36m"   # Light Cyan
+]
+random.shuffle(colors)
+
 result = ""
-for char in "Hello World!":
+text="Hello World!"
+for i in range(12):
     	for _ in range(5):
-    	   print(result + random.choice(string.ascii_letters), end='\r')
+    	   print(f"{result}{random.choice(colors)}{random.choice(string.ascii_letters)}", end='\r')
     	   time.sleep(0.1)
-    	result += char
+    	result+=colors[i]+text[i]
     	time.sleep(0.1)
-    	print(result,end='\r')
+print(result)
